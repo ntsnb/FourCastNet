0834546d683e56b7be756f91d2e11eb883a6c72a
ntsnb <90754175+ntsnb@users.noreply.github.com>
2025-02-10 10:46:22 +0800

    Add files via upload

---
+++ periodical.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+
+class RecurrentCycle(torch.nn.Module):
+    def __init__(self, cycle_len, channel_size):
+        super(RecurrentCycle, self).__init__()
+        self.cycle_len = cycle_len
+        self.channel_size = channel_size
+        self.data = torch.nn.Parameter(torch.zeros(cycle_len, channel_size), requires_grad=True)
+
+    # index: (batchsize, )
+    # gather_index: (batchsize, seq_len)
+    def forward(self, index, length):
+        gather_index = index.view(-1,1) + torch.arange(length, device = index.device).view(-1,1) % self.cycle_len
+        return self.data[gather_index]
+
+class Model(torch.nn.Module):
+    def __init__(self, configs):
+        super(Model, self).__init__()
+
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        self.enc_in = configs.enc_in
+        self.cycle_len = configs.cycle
+        self.model_type = configs.model_type
+        self.d_model = configs.d_model
+        self.use_revin = configs.use_revin
+
+        self.cycleQueue = RecurrentCycle(cycle_len=self.cycle_len, channel_size=self.enc_in)
+
+        assert self.model_type in ['linear', 'mlp']
+        if self.model_type == 'linear':
+            self.model = nn.Linear(self.seq_len, self.pred_len)
+        elif self.model_type == 'mlp':
+            self.model = nn.Sequential(
+                nn.Linear(self.seq_len, self.d_model),
+                nn.ReLU(),
+                nn.Linear(self.d_model, self.pred_len)
+            )
+
+
+    def forward(self, x, cycle_index):
+        # x: (batch_size, seq_len, enc_in)
+
+
+        if self.use_revin:
+            seq_mean = torch.mean(x, dim=1, keepdim=True)
+            seq_var = torch.var(x, dim=1, keepdim=True) + 1e-5
+            x = (x - seq_mean) / torch.sqrt(seq_var)
+
+        x = x - self.cycleQueue(cycle_index, self.seq_len)
+
+        y = self.model(x.permute(0, 2, 1)).permute(0, 2, 1)
+
+        y = y + self.cycleQueue((cycle_index + self.seq_len) % self.cycle_len, self.pred_len)
+
+        if self.use_revin:
+            y = y * torch.sqrt(seq_var) + seq_mean
+        
+        return y
